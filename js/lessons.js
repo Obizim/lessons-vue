3750e08ee71822a54dcf7b6c77cb32c0523b5199
Zim <obizimuzo@gmail.com>
2022-10-14 21:23:28 +0100

    header section

---
+++ js/lessons.js
@@ -0,0 +1,23 @@
+lessons: [
+    {
+      id: 1,
+      subject: "Mathematics",
+      location: "Hendon, London",
+      price: "£100",
+      spaces: 5,
+    },
+    {
+      id: 2,
+      subject: "Mathematics",
+      location: "Hendon, London",
+      price: "£100",
+      spaces: 5,
+    },
+    {
+      id: 3,
+      subject: "Mathematics",
+      location: "Hendon, London",
+      price: "£100",
+      spaces: 5,
+    },
+  ]
