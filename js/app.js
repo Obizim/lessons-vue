ef64a309e769925e302fd5c37585982e013708ae
Zim <obizimuzo@gmail.com>
2022-10-15 12:39:19 +0100

    search filter

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
-let rootContainer = document.getElementById('app')
-import { lessons } from './lessons.js'
+let rootContainer = document.getElementById("app");
+import { lessons } from "./lessons.js";
 
 let app = new Vue({
-    el: rootContainer,
-    data: function() {
-        return {
-            lessons
-        }
+  el: rootContainer,
+  data: function () {
+    return {
+      lessons,
+      cart: [],
+      search: "",
+    };
+  },
+  methods: {
+    addToCart(lesson) {
+      this.cart.push(lesson);
     },
-    methods: {
-        
-    }
-})
+  },
+  computed: {
+    filteredLessons() {
+      return this.lessons.filter(
+        (lesson) =>
+          lesson.subject.toLowerCase().includes(this.search.toLowerCase()) ||
+          lesson.location.toLowerCase().includes(this.search.toLowerCase())
+      );
+    },
+  },
+});
